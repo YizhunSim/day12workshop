3732f350b648bc2082287844f4f423b0265ed759
Yizhun <sim.yizhun@gmail.com>
2022-07-20 13:28:28 +0800

    day12workshop

---
+++ src/main/java/sg/edu/nus/iss/day12workshop/CartController.java
@@ -0,0 +1,41 @@
+package sg.edu.nus.iss.day12workshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
+import java.util.List;
+import java.util.ArrayList;
+
+
+@Controller
+@RequestMapping(path = { "/cart" })
+public class CartController {
+
+    @GetMapping(produces = { "text/html" })
+    public String displayCart(Model model) {
+        CartService cs = new CartService();
+        List<Item> cartItems = cs.getShoppingItems();
+        model.addAttribute("cart", cartItems);
+
+        return "cart";
+    }
+
+    @GetMapping("{itemname}")
+    public String filteredCart(
+        @PathVariable(name="itemname", required=true) String itemname,
+        Model model) {
+        CartService cs = new CartService();
+        List<Item> cartItems = cs.getShoppingItems();
+
+        List<Item> filteredItems = new ArrayList<>();
+        for (Item s : cartItems) {
+            if (s.getItemName().contains(itemname)) {
+                filteredItems.add(s);
+            }
+        }
+
+        model.addAttribute("cart", filteredItems);
+
+        return "cart";
+    }
+}
